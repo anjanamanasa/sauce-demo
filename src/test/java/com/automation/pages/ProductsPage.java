460c4e33956f29280f2e34ccd7b646f929c2964b
anjan <anjana.manasa@gmail.com>
2024-11-01 10:45:14 +0000

    Add products to cart and checkout

--- src/test/java/com/automation/pages/ProductsPage.java
+++ src/test/java/com/automation/pages/ProductsPage.java
@@ -1,8 +1,22 @@
 package com.automation.pages;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 public class ProductsPage extends Page{
+
+    @FindBy(xpath = "//div[@id='shopping_cart_container']//span")
+    private WebElement cartSize;
+
+
+    @FindBy(xpath = "//div[@id='shopping_cart_container']//a")
+    private WebElement cart;
+
+    @FindBy(xpath = "//div[@class='inventory_container']//div//div[@class='inventory_item']")
+    private List<WebElement> items;
 
     public ProductsPage(ChromeDriver driver) {
         super(driver);
@@ -10,6 +24,18 @@ public class ProductsPage extends Page{
 
     public String getTitle() {
         return driver.getTitle();
+    }
+
+    public List<WebElement> getItems() {
+        return items;
+    }
+
+    public Integer getCartSize() {
+        return Integer.parseInt(cartSize.getText());
+    }
+
+    public void clickOnTheCart() {
+        cart.click();
     }
 
 }
